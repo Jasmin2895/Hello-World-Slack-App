fb1033445f063b68aff37b392bcf28ab2a4f3b65
Jasmin2895 <jasmin28virdi@gmail.com>
2022-02-13 22:17:13 +0530

    app home page displayed

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
 require('dotenv').config();
 
 const {App} = require('@slack/bolt');
-const store = require('./store');
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGINING_SECRET
 })
 
-app.event('app_home_opened', async ({event, say})=> {
-    // Look up the user from the DB
-
-    if(event?.user){
-
-        await say(`Hello world, and welcome <@${event.user}>!`)
-    }else {
-        
-        await say('Hi again!')
+// Listen to the app_home_opened Events API event to hear when a user opens your app from the sidebar
+app.event("app_home_opened", async ({ payload, client }) => {
+    const userId = payload.user;
+    
+    try {
+      // Call the views.publish method using the WebClient passed to listeners
+      const result = await client.views.publish({
+        user_id: userId,
+        view: {
+          // Home tabs must be enabled in your app configuration page under "App Home"
+          "type": "home",
+          "blocks": [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome home, <@" + userId + "> :house:*"
+              }
+            },
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>."
+              }
+            },
+            {
+              "type": "divider"
+            },
+            {
+              "type": "context",
+              "elements": [
+                {
+                  "type": "mrkdwn",
+                  "text": "Psssst this home tab was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+                }
+              ]
+            }
+          ]
+        }
+      });
+  
+      console.log(result);
+    }
+    catch (error) {
+      console.error(error);
     }
 });
 
